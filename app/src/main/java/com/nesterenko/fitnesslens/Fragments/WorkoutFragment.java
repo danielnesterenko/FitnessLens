f7e398e986051dc2680e7460e4d7f7dc1e0e09e6
DanielNesterenko <daniel.nesterenko1997@gmail.com>
2022-10-07 10:55:36 +0200

    Fixed Bug when clearing UserData manually.

--- app/src/main/java/com/nesterenko/fitnesslens/Fragments/WorkoutFragment.java
+++ app/src/main/java/com/nesterenko/fitnesslens/Fragments/WorkoutFragment.java
@@ -26,10 +26,12 @@ import com.nesterenko.fitnesslens.RecyclerViews.RecyclerViewAdapterWorkout;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class WorkoutFragment extends Fragment {
